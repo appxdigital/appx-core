f3d22926c3490a9ab7b0611e88d3f7445f6c3038
David <joao.david@appx.pt>
2024-11-18 14:40:21 +0000

    Moved dependencies to peerDependencies; Changed storage of fileupload config to env.development

--- src/index.ts
+++ src/index.ts
@@ -20,3 +20,4 @@ export * from './common/interfaces/storage-service.interface';
 export * from './modules/file/file-upload.controller';
 export * from './modules/file/file-upload.service';
 export * from './common/decorators/permission.decorator';
+export * from './common/interceptors/file.interceptor';
